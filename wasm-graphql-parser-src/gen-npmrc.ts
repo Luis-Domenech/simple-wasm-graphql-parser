1044f3e7dcf9244646613c9d4d67cd2a36149fc8
Luis-Domenech <luisfabiandomenech@gmail.com>
2022-07-27 00:25:28 -0400

    Added logic for loggin in to npm

--- wasm-graphql-parser-src/gen-npmrc.ts
+++ wasm-graphql-parser-src/gen-npmrc.ts
@@ -10,19 +10,23 @@ const gen_npmrc = async () => {
     console.error("NPM_USER is not set in environment")
     process.exit(1)
   }
+  if (!process.env.NPM_EMAIL) {
+    console.error("NPM_EMAIL is not set in environment")
+    process.exit(1)
+  }
   // Create fodler if it doesn't exist
-  fs.mkdirSync(path.join(__dirname, "./pkg"), { recursive: true })
+  // fs.mkdirSync(path.join(__dirname, "./pkg"), { recursive: true })
 
   let file_content = [
-    `registry=https://registry.npmjs.org/`,
-    ``,
-    `@${process.env.NPM_USER}:registry=https://registry.npmjs.org/`,
     `//registry.npmjs.org/:_authToken=${process.env.NPM_TOKEN}`,
-    `always-auth=true`
+    `email=${process.env.NPM_EMAIL}`,
+    `always-auth=true`,
+    // `registry = https://registry.npmjs.org/`,
+    `@${process.env.NPM_USER}:registry=https://registry.npmjs.org/`,
   ].join("\n")
 
   try {
-    fs.writeFileSync(path.join(__dirname, "./pkg/.npmrc"), file_content)
+    fs.writeFileSync("~/.npmrc", file_content, {flag: "a+"})
   }
   catch(e) {
     console.error(e)
