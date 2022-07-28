7b6628f8599e15abb547b21528af905225e9d080
Luis-Domenech <luisfabiandomenech@gmail.com>
2022-07-27 23:51:00 -0400

    Fixed script

--- wasm-graphql-parser-src/mod-package-json.ts
+++ wasm-graphql-parser-src/mod-package-json.ts
@@ -5,6 +5,10 @@ const mod_package_json = async () => {
     console.error("NODE_AUTH_TOKEN and NPM_TOKEN are neither set in environment. Set one of them before deploying to prod.")
     process.exit(1)
   }
+  // if (!process.env.NPM_USER) {
+  //   console.error("NPM_USERis not set in environment")
+  //   process.exit(1)
+  // }
 
   let file_content = fs.readFileSync("./pkg/package.json", 'utf-8')
 
@@ -12,6 +16,7 @@ const mod_package_json = async () => {
     `,`,
     `  "publishConfig": {`,
     // `    "registry": "https://registry.npmjs.org/:_authToken=$${process.env.NODE_AUTH_TOKEN ? 'process.env.NODE_AUTH_TOKEN' : 'process.env.NPM_TOKEN'}"`,
+    // `    "${process.env.NPM_USER}:registry": "https://registry.npmjs.org/"`,
     `    "registry": "https://registry.npmjs.org/"`,
     `  }`,
     `}`
